61fd2db24b070cdf2f152a4714fc5d75d5f228c0
flores-joice <flores.joice@gmail.com>
2019-03-01 17:53:49 -0300

    já mapiei os valores

--- online-datawash/src/components/nav-vertical.js
+++ online-datawash/src/components/nav-vertical.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import './components.css'
 import { apiMenu } from '../infra/api-config';
-import { Consulta, Enriquecimento, Higienizacao, EnriquecimentoDrop, Gerencial, GerencialDrop, Monitoramento, MonitoramentoDrop, Relatario, RelatarioDrop } from '../components/itensMenu'
-
+import {
+  Consulta, Enriquecimento, Higienizacao,
+  EnriquecimentoDrop, Gerencial, GerencialDrop, Monitoramento,
+  MonitoramentoDrop, Relatario, RelatarioDrop
+} from '../components/itensMenu'
 
 export default class NavVertical extends Component {
   constructor(props) {
@@ -11,54 +14,109 @@ export default class NavVertical extends Component {
       // armazenar o token para novas requisiçoes de endpoint
       token: '',
       usuarioLogado: false,
-      funcionalidades: []
-
+      funcionalidades: [],
+      valor: [],
+      lista: []
     }
   }
 
   componentDidMount = async () => {
     try {
-      //cria uma constante que pega o valor do token de localstorage
       const token = await localStorage.getItem('auth-token', token);
 
       if (token) {
-        console.log(token)
         this.setState({ usuarioLogado: true }, () => {
         })
         const response = await apiMenu.get()
+        //item armazena o valor da response retornada da api
         const item = response.data
-        for (let i = 0; i >= item.length; i++) {
-          if (item === "1") {
-            return (
-              console.log('ok')
-              
-              )
-            }
+
+        //FOR = lista todos os valores contido em item
+        for (let [key, value] of item.entries(item)) {
+
+          //aqui atribuimos os valores listados do FOR na variavel valor
+          this.setState({ valor: value })
+          const newFunc = []
+          //fazemos a comparação dos valores, se true, 
+          //ele envia uma string para outra variavel
+          {
+            //se o valor contido na variavel for = 1 ele execura, 
+            //senão nao faz nada
+            this.state.valor === "1" &&
+              (newFunc = this.state.funcionalidades.concat('consulta')) &&
+              (this.setState({ funcionalidades: newFunc }))
           }
-          console.log(item)
-        }
+          {
+            this.state.valor === "2" &&
+              (newFunc = this.state.funcionalidades.concat('enriquecimento')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "3" &&
+              (newFunc = this.state.funcionalidades.concat('enriquecimentoDrop')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "4" &&
+              (newFunc = this.state.funcionalidades.concat('higienizacao')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "5" &&
+              (newFunc = this.state.funcionalidades.concat('gerencial')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "6" &&
+              (newFunc = this.state.funcionalidades.concat('gerencialDrop')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "7" &&
+              (newFunc = this.state.funcionalidades.concat('monitoramento')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "8" &&
+              (newFunc = this.state.funcionalidades.concat('monitoramentoDrop')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "9" &&
+              (newFunc = this.state.funcionalidades.concat('relatario')) &&
+              (this.setState({ funcionalidades: newFunc }))
+          }
+          {
+            this.state.valor === "10" &&
+              (newFunc = this.state.funcionalidades.concat('relatarioDrop')) &&
+              (this.setState({ funcionalidades: newFunc }))
+              console.log(this.state.funcionalidades)
+          }
+        };
+      }
+    
+      //cria uma constante que pega o valor do token de localstorage
     } catch (response) {
-      // const error = responseMenu.originalError;
-      // const messageError = error.response
       console.log('messageError', response)
     }
   }
 
   render() {
-    const { item } = this.state;
     return (
-      <div className='nav-vertical bg-dark d-flex align-items-center'>
+      <div className='nav-vertical bg-dark d-flex align-items-center' >
         <div className='w-100'>
-        <p>pegar o valor da lista</p>
-          {/* {item.teste && <Consulta/>} */}
-          {/* {item.enriquecimento ? <Enriquecimento /> : null}
-          {item.enriquecimentoDrop ? <EnriquecimentoDrop /> : null}
-          {item.gerencial ? <Gerencial /> : null}
-          {item.gerencialDrop ? <GerencialDrop /> : null}
-          {item.monitoramento ? <Monitoramento /> : null}
-          {item.monitoramentoDrop ? <MonitoramentoDrop /> : null}
-          {item.relatorio ? <Relatario /> : null}
-          {item.relatorioDrop ? <RelatarioDrop /> : null} */}
+          <p className='text-light'>pegar o valor da funcionalidades</p>
+          <p className='text-light'>passar o valor para o render</p>
+          {this.state.funcionalidades === "consulta" && (<Consulta />)}
+          {this.state.funcionalidades === "enriquecimento" && (<Enriquecimento />)}
+          {this.state.funcionalidades === "enriquecimentoDrop" && (<EnriquecimentoDrop />)}
+          {this.state.funcionalidades === "higienizacao" && (<Higienizacao />)}
+          {this.state.funcionalidades === "gerencial" && (<Gerencial />)}
+          {this.state.funcionalidades === "gerencialDrop" && (<GerencialDrop />)}
+          {this.state.funcionalidades === "monitoramento" && (<Monitoramento />)}
+          {this.state.funcionalidades === "monitoramentoDrop" && (<MonitoramentoDrop />)}
+          {this.state.funcionalidades === "relatario" && (<Relatario />)}
+          {this.state.funcionalidades === "relatarioDrop" && (<RelatarioDrop />)}
         </div>
       </div >
     )
